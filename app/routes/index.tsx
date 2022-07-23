1050ffdb6bb3985cc40a4e09f593d0f0d65c2d27
Mark Njoroge <marknjo7@gmail.com>
2022-07-23 16:56:09 +0300

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,32 @@
+export default function Index() {
+  return (
+    <div className="grid justify-center fontFamily['system-ui, sans-serif'] lending-6 text-gray-800">
+      <h1 className="text-4xl mb-4">Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
